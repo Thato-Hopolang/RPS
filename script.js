bedd9b009b31107bdbadeae1f34a990c9e7ce623
Thato Botsane <thbjr@outlook.com>
2023-08-09 11:06:52 +0200

    same as late

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const computerChoiceDisplay = document.getElementById("computerChoice");
 const userChoiceDisplay = document.getElementById("playerChoice");
 const resultDisplay = document.getElementById("resultDisplay");
+const ruleDisplay = document.getElementById("ruleDisplay");
+const grandFinaleDisplay = document.getElementById("grandFinaleDisplay");
 
 const possibleChoices = document.querySelectorAll("button");
 
@@ -8,9 +10,11 @@ const playerWinsDisplay = document.getElementById("playerWinsDisplay");
 const computerWinsDisplay = document.getElementById("computerWinsDisplay");
 const roundsPlayedDisplay = document.getElementById("roundsPlayedDisplay");
 
+let law;
 let playerChoice;
 let computerChoice;
 let result;
+let final;
 let playerScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
@@ -23,6 +27,17 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click
 }));
 
 
+//At Start
+document.getElementById("grandFinaleDisplay").hidden = true;
+document.getElementById("optionYes").hidden = true;
+document.getElementById("optionNo").hidden = true;
+
+
+function theEnd() {
+    document.getElementById("grandFinaleDisplay").hidden = false;
+    document.getElementById("optionYes").hidden = false;
+    document.getElementById("optionNo").hidden = false;
+}
 
 function getComputerChoice () {
     let x = Math.floor(Math.random() * 3)
@@ -42,8 +57,6 @@ function getComputerChoice () {
 
 
 function getResult() {
-    while (playerScore > 5 && computerScore > 5) {
-
         if (computerChoice == playerChoice) {
             result = "Draw!";
             roundsPlayed = roundsPlayed +1;
@@ -78,9 +91,46 @@ function getResult() {
             computerScore = computerScore +1;
             roundsPlayed = roundsPlayed +1;
         }
+        if (computerScore === 5) {
+            final = "Computer has won the tournament."
+            law = "Would like to play again?"
+            finale()
+            theEnd()
+        } else if (playerScore === 5) {
+            final = "Player has won the tournament."
+            law = "Would like to play again?"
+            finale()
+            theEnd()
+        }
     
         resultDisplay.innerHTML = result;
         computerWinsDisplay.innerHTML = computerScore;
         roundsPlayedDisplay.innerHTML = roundsPlayed;
         playerWinsDisplay.innerHTML = playerScore;
+        grandFinaleDisplay.innerHTML = final;
+        ruleDisplay.innerHTML = law;
 };
+
+function finale() {
+    document.getElementById("computerChoice").hidden = true;
+    document.getElementById("playerChoice").hidden = true;
+    document.getElementById("computerWinsDisplay").hidden = true;
+    document.getElementById("roundsPlayedDisplay").hidden = true;
+    document.getElementById("playerWinsDisplay").hidden = true;
+    document.getElementById("Rock").hidden = true;
+    document.getElementById("Paper").hidden = true;
+    document.getElementById("Scissors").hidden = true;
+    document.getElementById("playerInfo").hidden = true;
+    document.getElementById("playerOptions").hidden = true;
+    document.getElementById("computerInfo").hidden = true;
+    document.getElementById("computerOptions").hidden = true;
+    document.getElementById("roundInfo").hidden = true;
+    document.getElementById("resultDisplay").hidden = true;
+
+
+};
+
+
+function yes() {};
+
+function no() {};
